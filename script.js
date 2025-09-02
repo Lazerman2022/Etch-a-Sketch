ac86aa3b77ba4c15bbcbfb3caceef892d2927560
Cagatay <122673439+Lazerman2022@users.noreply.github.com>
2025-09-02 19:09:22 +0300

    fixed the grid coloring behaviour

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-//todo add size adjustable grid
+
 const grid = document.querySelector(".grid");
 
-function createGrid(gridRow) {
-  for (let r = 0; r < gridRow; r++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    grid?.appendChild(row);
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
-    for (let c = 0; c < gridRow; c++) {
-      const gridDiv = document.createElement("div");
-      gridDiv.classList.add("grid-block");
-      gridDiv.textContent = `(${r},${c})`;
-      row.appendChild(gridDiv);
+function createGrid(gridCol) {
+
+    const blockSize = 960 / gridCol;
+
+    for (let c = 0; c < gridCol; c++) {
+        const col = document.createElement("div");
+        col.classList.add("col");
+        grid?.appendChild(col);
+
+        for (let r = 0; r < gridCol; r++) {
+
+            const gridDiv = document.createElement("div");
+            gridDiv.classList.add("grid-block");
+
+            gridDiv.style.width = `${blockSize}px`;
+            gridDiv.style.height = `${blockSize}px`;
+
+            gridDiv.addEventListener("mousedown", (e) => {})
+            gridDiv.addEventListener("mouseup", (e) => {})
+            gridDiv.addEventListener("dragstart", (e) => e.preventDefault());
+            gridDiv.addEventListener("mouseover", (e) => {
+                if (mouseDown) {
+                    gridDiv.classList.toggle("active");
+                }
+            })
+
+            col.appendChild(gridDiv);
+        }
     }
-  }
 }
-
-createGrid(4);
+createGrid(32);
